a72e1f0579849cf3b573e292b72298c83227ed84
lemming13 <patrikprok48@gmail.com>
2025-04-21 18:19:25 +0300

    Update newsData.js

--- newsData.js
+++ newsData.js
@@ -1,1 +1,14 @@
+const newsData = [
+    {
+        id: 1,
+        title: "Запуск сайта",
+        text: "Мы запустили бета-версию lemin.site! Это cайт нового поколения.",
+        date: "21.04.2025",
+        image: "LemChat.png"
+    }
+];
 
+// Экспортируем массив новостей (для Vercel нам не нужен настоящий export, но оставим для совместимости)
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = newsData;
+}
